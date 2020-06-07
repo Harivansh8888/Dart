7d3b26a133100c20394d35779174078d4cc5f731
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-06-08 00:57:18 +0530

    strings in dart

---
+++ string.dart
@@ -0,0 +1,32 @@
+main() {
+  var s1 = 'Hello';
+  var s2 = "Hi";
+  var s3 = 'It\'s Monday';
+  var s4 = "It's raining now";
+
+
+  print(s1);
+  print(s2);
+  print(s3);
+  print(s4);
+  print('');
+
+  //Raw String
+  var s = r'It is a \n raw string';
+  print(s);
+
+  var age = 20;
+  var str = 'My age is $age';
+  print(str);
+
+  var str1 = '''
+  Hello guys
+  I am Harivansh''';
+  print(str1);
+
+  var str2 = """
+  Hey everyone mutli line
+  comment example 2""";
+  print(str2);
+
+}
