f700072573baf6271c8b4d714176d6ca79bc9733
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-06-08 01:15:44 +0530

    constant type

---
+++ constant.dart
@@ -0,0 +1,15 @@
+main() {
+  const aConstNum = 10;
+  const aConstBool = true;
+  const aConstString = 'Harivansh';
+
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+
+
+}
