3aed185d6fb2584ab6b197d85ca0ac59f22de6a1
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-06-10 01:08:32 +0530

    switch case implemented

---
+++ switch.dart
@@ -0,0 +1,16 @@
+main() {
+  int h = 1;
+
+
+  switch(h) {
+    case 0:
+      print('Hi');
+      break;
+    case 1:
+      print('Hello');
+      break;
+    case 2:
+      print('world');
+      break;
+  }
+}
