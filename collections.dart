a6522c5f8076019ea3da36fd5fc9ce607136aa76
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-06-12 20:49:29 +0530

    assigning list to other variable

--- collections.dart
+++ collections.dart
@@ -7,4 +7,13 @@ main() {
   print(names[0]);
   print(names.length);
 
+  List n = ['Jack', 'Jill'];
+
+  var n2 = n; //Does not copy , only assigns
+
+  n[1] = 'Mark';
+
+  for (var i in n2) {
+    print(i);
+  }
 }
