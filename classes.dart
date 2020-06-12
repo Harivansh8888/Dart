760389154ad9c033e8d8fa66de49dbd5ad3f596f
Harivansh Mathur <46771504+Harivansh8888@users.noreply.github.com>
2020-06-12 21:32:08 +0530

    main function

--- classes.dart
+++ classes.dart
@@ -19,3 +19,24 @@ class X {
 
   X(this.fname);
 }
+
+void main() {
+  Person person1 = Person('Harsh');
+  Person person2 = Person('Harmit', 20);
+
+  //person1.name = 'Jatin';
+  //person1.age = 19;
+  person1.showOutput();
+  person2.showOutput();
+
+
+
+  //Class X  object
+  var x = X('Jack');
+  print(x.fname);
+
+  //x.fname = 'Jill'; ----> This will raise an error because of the use of final keyword
+  print(x.fname);
+
+  print(X.age);
+}
